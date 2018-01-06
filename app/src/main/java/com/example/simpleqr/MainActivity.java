ba7a48e4987a6b37a341af7ab12a0df761a4a69c
twosheep0603 <mike830603@gmail.com>
2018-01-06 19:20:07 +0800

    Create Files

---
+++ app/src/main/java/com/example/simpleqr/MainActivity.java
@@ -0,0 +1,50 @@
+package com.example.simpleqr;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import android.app.Activity;
+import com.google.zxing.integration.android.*;
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
+import android.widget.Toast;
+
+public class MainActivity extends AppCompatActivity {
+   private Activity mainactivity;
+    private TextView scan_content;
+    private TextView scan_format;
+    private Button scan_btn;
+    @Override
+    protected void onCreate(Bundle saveInstanceStat){
+        super.onCreate(saveInstanceStat);
+        setContentView(R.layout.activity_main);
+        init_view();
+        scan_btn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                IntentIntegrator scanIntegrator = new IntentIntegrator(mainactivity);
+                scanIntegrator.initiateScan();
+            }
+        });
+    }
+    public void onActivityResult(int requestCode, int resultCode, Intent intent){
+        IntentResult scanningResult = IntentIntegrator.parseActivityResult(requestCode, resultCode, intent);
+        if(scanningResult!=null){
+            String scanContent=scanningResult.getContents();
+            String scanFormat=scanningResult.getFormatName();
+            scan_content.setText(scanContent);
+            scan_format.setText(scanFormat);
+        }else{
+            Toast.makeText(getApplicationContext(),"nothing",Toast.LENGTH_SHORT).show();
+        }
+    }
+    private void init_view(){
+        this.scan_content=(TextView)findViewById(R.id.scan_content);
+        this.scan_format=(TextView)findViewById(R.id.scan_format);
+        this.mainactivity=this;
+        this.scan_btn = (Button)findViewById(R.id.scan_btn);
+    }
+}
